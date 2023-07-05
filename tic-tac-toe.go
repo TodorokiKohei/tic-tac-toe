cb08abe3c758be3ffbf852d533ec4ae744209172
Kohei Todoroki <m222408@hiroshima-u.ac.jp>
2023-07-05 02:25:43 +0000

    Check invalid operation

--- tic-tac-toe.go
+++ tic-tac-toe.go
@@ -1,15 +1,26 @@
 package main
 
+import "errors"
+
 type Board struct {
 	tokens []int
 }
 
-func (b *Board) put(x, y int, u string) {
-	if u == "o" {
-		b.tokens[x+3*y] = 1
-	} else if u == "x" {
-		b.tokens[x+3*y] = -1
+func (b *Board) put(x, y int, u string) error {
+	if (0 <= x && x <= 2) || (0 <= y && y <= 2) {
+		if (u != "x" && u != "o") || b.tokens[x+3*y] != 0 {
+			// ox以外の入力, 既に記入されているマスへの入力
+			return errors.New("Invalid operation")
+		}
+		if u == "o" {
+			b.tokens[x+3*y] = 1
+		} else if u == "x" {
+			b.tokens[x+3*y] = -1
+		}
+		return nil
 	}
+	// 配列外参照
+	return errors.New("Out of range")
 }
 
 func (b *Board) get(x, y int) string {
@@ -33,6 +44,15 @@ func (b *Board) judge() bool {
 		if sum == 3 || sum == -3 {
 			return true
 		}
+	}
+	// 斜め判定
+	sum := b.tokens[0] + b.tokens[4] + b.tokens[8]
+	if sum == 3 || sum == -3 {
+		return true
+	}
+	sum = b.tokens[2] + b.tokens[4] + b.tokens[6]
+	if sum == 3 || sum == -3 {
+		return true
 	}
 	return false
 }
--- tic-tac-toe_test.go
+++ tic-tac-toe_test.go
@@ -57,3 +57,52 @@ func TestWinLose03(t *testing.T) {
 		t.Errorf("Test fail expected: %t, result: %t\n", true, b.judge())
 	}
 }
+
+func TestWinLose04(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	// 斜め列で勝負がついた状態での判定
+	b.put(0, 0, "o")
+	b.put(1, 1, "o")
+	b.put(2, 2, "o")
+	if !b.judge() {
+		t.Errorf("Test fail expected: %t, result: %t\n", true, b.judge())
+	}
+}
+
+func TestWinLose05(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	// 斜め列で勝負がついた状態での判定
+	b.put(0, 2, "x")
+	b.put(1, 1, "x")
+	b.put(2, 0, "x")
+	if !b.judge() {
+		t.Errorf("Test fail expected: %t, result: %t\n", true, b.judge())
+	}
+}
+
+func TestInvalidOperation01(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	// 配列外操作
+	err := b.put(4, 4, "x")
+	if err == nil {
+		t.Errorf("Invalid Operation")
+	}
+
+	// 重複操作
+	err = b.put(1, 1, "x")
+	if err != nil {
+		if err == nil {
+			t.Errorf("Test fail %T\n", err)
+		}
+	}
+	err = b.put(1, 1, "o")
+	if err == nil {
+		t.Errorf("Invalid Operation")
+	}
+}
